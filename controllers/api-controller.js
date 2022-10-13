b39225293bf8a09fe66ef085e160306b7b3f16df
Kamakazee <cam_va@yahoo.co.uk>
2022-10-13 14:53:45 +0100

    Removed console.logs

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -2,7 +2,6 @@ const { selectAPI } = require("../models/api-model.js");
 
 const getAPI = (request, response)=>{
 
-    console.log("Inside api controller")
     selectAPI().then((api)=>{
         response.status(200).send({api})
     })
--- models/api-model.js
+++ models/api-model.js
@@ -2,7 +2,6 @@ const { dir } = require("console");
 const fs = require("fs/promises");
 
 const selectAPI = () => {
-  console.log("Inside api model");
 
   return fs.readFile("./endpoints.json", "utf-8").then((data) => {
 
